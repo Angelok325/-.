1677c96a1f20e1718192b1d462574ee1f9613762
Angelok325 <146517814+Angelok325@users.noreply.github.com>
2024-12-22 22:02:47 +0300

    Add files via upload

---
+++ лаба/лаба/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] ma = { 2, 3, 5, 1, 4 };
+        int result = Sum(ma);
+        Console.WriteLine("Сумма элементов массива = " + result);
+
+    }
+    public static int Sum(int[] ma)
+    {
+        int TotalSum = 0;
+        foreach (int i in ma)
+        {
+            TotalSum += i;
+        }
+        return TotalSum;
+
+    }
+}
